2f9feb6057dc0e8eb1a2394bfa94229146a1a255
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-02-12 22:34:28 -0800

    'second-commit'

--- script.js
+++ script.js
@@ -12,30 +12,32 @@ const radio3 = document.querySelector(".radio--3");
 const radio4 = document.querySelector(".radio--4");
 const submit = document.querySelector(".submit");
 const container = document.querySelector(".quiz-cont");
+const correctAns = document.querySelector(".corrAns");
+
 let questionArr = [];
 let ques = {};
-
+let numCorrect = 0;
 const generateMarkup = function () {
   const markup = questionArr
-    .map((el) => {
+    .map((el, i) => {
       return `
     <div class="submit-cont">
       <p>${el.question}</p>
       <div class="choises">
-          <input class="radio--1" type="radio" name="choose" id="choise1" />
-          <label for="choise1">${el.ans1}</label>
+          <input  class="radio radio--1" type="radio" name="choose${i}" id="choise1"  value="${el.answer.ans1}" />
+          <label for="choise1">${el.answer.ans1}</label>
       </div>
       <div class="choises">
-          <input class="radio--2" type="radio" name="choose" id="choise2" />
-          <label for="choise2">${el.ans2}</label>
+          <input  class="radio radio--2" type="radio" name="choose${i}" id="choise2" value="${el.answer.ans2}" />
+          <label for="choise2">${el.answer.ans2}</label>
       </div>
       <div class="choises">
-          <input class="radio--3" type="radio" name="choose" id="choise3" />
-          <label for="choise3">${el.ans3}</label>
+          <input  class="radio radio--3" type="radio" name="choose${i}" id="choise3" value="${el.answer.ans3}" />
+          <label for="choise3">${el.answer.ans3}</label>
       </div>
       <div class="choises">
-          <input class="radio--4" type="radio" name="choose" id="choise4" />
-          <label for="choise4">${el.ans4}</label>
+          <input  class="radio radio--4" type="radio" name="choose${i}" id="choise4" value="${el.answer.ans4}" />
+          <label for="choise4">${el.answer.ans4}</label>
       </div>
       </div>
     `;
@@ -43,24 +45,47 @@ const generateMarkup = function () {
     .join("");
   return markup;
 };
+function showResults() {}
 btnAdd.addEventListener("click", function () {
   ques = {
     question: question.value,
-    ans1: choise1.value,
-    ans2: choise2.value,
-    ans3: choise3.value,
-    ans4: choise4.value,
+    answer: {
+      ans1: choise1.value,
+      ans2: choise2.value,
+      ans3: choise3.value,
+      ans4: choise4.value,
+    },
+    correctAnswer: correctAns.value,
   };
   questionArr.push(ques);
+
   choise1.value =
     choise2.value =
     choise3.value =
     choise4.value =
     question.value =
+    correctAns.value =
       "";
   container.innerHTML = "";
   container.insertAdjacentHTML("afterbegin", generateMarkup());
 });
 
 submit.addEventListener("click", (e) => {
+  const answers = document.querySelectorAll(".radio");
+  const submitCont = document.querySelector(".submit-cont");
+  questionArr.forEach((currAns, quesNum) => {
+    const selector = document.querySelectorAll(`input[name=choose${quesNum}]`);
+    let userAns = "";
+    selector.forEach((el) => {
+      if (el.checked) {
+        userAns = el.value;
+        if (userAns === currAns.correctAnswer) {
+          numCorrect++;
+          submitCont.style.backgroundColor = "green";
+        } else {
+          submitCont.style.backgroundColor = "orangered";
+        }
+      }
+    });
+  });
 });
